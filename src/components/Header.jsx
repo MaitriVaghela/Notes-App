4c9e4081366534ce5e48e1eab3aae1be776841f7
Maitri Vaghela <vaghela.maitri@gmail.com>
2020-05-31 14:50:58 -0400

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+/* This jsx file deals with the header section of the website.*/
+
+import React from "react";
+import HighlightIcon from "@material-ui/icons/Highlight";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <HighlightIcon />
+        Notes
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
